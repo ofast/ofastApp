0fb21bc99552a8f5b18bcce35c3a7b6c93b302d5
An Khang <anhkhanguit@gmail.com>
2016-03-25 14:34:27 +0700

    Update link

--- app/src/main/java/com/hung/ofastapp/CreateConnection/ofastURL.java
+++ app/src/main/java/com/hung/ofastapp/CreateConnection/ofastURL.java
@@ -4,10 +4,11 @@ package com.hung.ofastapp.CreateConnection;
  * Created by Hung on 11/24/2015.
  */
 public class ofastURL {
-    public static String login = "http://ofast.esy.es/frontend/web/index.php?r=app%2Flogin";
-    public static String register = "http://ofast.esy.es/frontend/web/index.php?r=app%2Fsignup";
-    public static String brand_product ="http://ofast.esy.es/frontend/web/index.php?r=catalog%2Fapplist";
-    public static String brand = "http://ofast.esy.es/frontend/web/index.php?r=provider%2Findex";
-    public static String brand_image = "http://ofast.esy.es/backend/web/uploads/";
+    public static String login = "http://ofast.esy.es/site/login";
+    public static String register = "http://ofast.esy.es/site/signup";
+    public static String brand_product ="http://ofast.esy.es/catalog/applist";
+    public static String brand = "http://ofast.esy.es/provider/index";
+    public static String brand_image = "http://ofast.esy.es/admin/uploads/";
     public static String frontend_Web_image = "http://ofast.esy.es/frontend/web/images/";
+    public static String order = "http://ofast.esy.es/cart/app";
 }
